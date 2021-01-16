3e8e590eb6d1a22f65c0d04ab7ce8ffc1def4a27
brandon lindberg <lindberg.scott.brandon@gmail.com>
2021-01-16 19:31:50 +0900

    added destroy method for doses

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,5 +1,6 @@
 class Dose < ApplicationRecord
   validates :description, presence: true
-  belongs_to :cocktail 
+  validates :cocktail_id, uniqueness: { scope: :ingredient_id }
+  belongs_to :cocktail
   belongs_to :ingredient
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,13 +5,49 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# require 'json'
+require 'httparty'
+
 puts "cleaning database"
 Cocktail.destroy_all
 
 puts "creating cocktails...."
+
 5.times do
   Cocktail.create(
     name: Faker::Games::SuperMario.character
   )
 end
+
+
 puts "cocktails created!!"
+
+
+
+file = HTTParty.get('https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list')
+ingredient_hash = JSON.parse(file.body)
+
+
+puts "cleaning database"
+
+Ingredient.destroy_all
+
+puts "creating Ingredients...."
+
+10.times do
+  Ingredient.create(name: ingredient_hash["drinks"].sample["strIngredient1"])
+end
+
+puts "Ingredients created!!"
+
+
+puts "cleaning database"
+Dose.destroy_all
+
+puts "creating doses...."
+
+
+5.times do
+  Dose.create(description: "#{rand(1..5)} parts")
+end
+puts "Doses created!!"
